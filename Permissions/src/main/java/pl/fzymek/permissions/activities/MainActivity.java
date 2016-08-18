5d34b583e5c8ecd2fec4606bc6dbb1b516eb825d
Filip <filip@MacBook-Pro-Filip.local>
2016-08-18 12:45:19 +0200

    Add blank activity

---
+++ Permissions/src/main/java/pl/fzymek/permissions/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package pl.fzymek.permissions.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import pl.fzymek.permissions.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
