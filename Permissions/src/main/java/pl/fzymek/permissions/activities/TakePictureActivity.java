9d548abd109ec8613fb0dd1b7f17ef8d6daf9929
Filip Zymek <filip.zymek@gmail.com>
2016-08-19 13:34:00 +0200

    Remove unused class

--- Permissions/src/main/java/pl/fzymek/permissions/activities/TakePictureActivity.java
+++ Permissions/src/main/java/pl/fzymek/permissions/activities/TakePictureActivity.java
@@ -8,13 +8,11 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.widget.Toast;
 
 import net.bozho.easycamera.DefaultEasyCamera;
 import net.bozho.easycamera.EasyCamera;
@@ -30,7 +28,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import pl.fzymek.permissions.R;
-import pl.fzymek.permissions.utils.SystemUI;
 import timber.log.Timber;
 
 public class TakePictureActivity extends AppCompatActivity implements SurfaceHolder.Callback {
