8b802f3a30b9a25babc1023d70628f58eef14ee2
Filip Zymek <filip.zymek@gmail.com>
2016-09-28 17:16:16 +0200

    Rename package

--- mvp-imagegallery/src/main/java/pl/fzymek/imagegallery/MainActivity.java
+++ mvp-imagegallery/src/main/java/pl/fzymek/imagegallery/MainActivity.java
@@ -1,7 +1,9 @@
-package pl.fzymek.mvp_catgallery;
+package pl.fzymek.imagegallery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import pl.fzymek.mvp_catgallery.R;
 
 public class MainActivity extends AppCompatActivity {
 
